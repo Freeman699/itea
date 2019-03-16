af984d32dc6ffabdc552b53b49ddc3336cb77464
Freeman699 <mr.frimen1324@gmail.com>
2019-03-16 19:39:48 +0200

    Integer leteral

--- Literal.cpp
+++ Literal.cpp
@@ -1,8 +1,17 @@
 #include <cstdio>
+#include <iostream>
+
+using namespace std;
 
 int main(void)
 {
-    printf("\\0");
+    printf("Integer literal E4 (HEX):%d\n",0xE4);
+    printf("Integer literal 1337 (DEC):%d\n",1337);
+    printf("Integer literal 0502 (OCT):%d\n",0502);
+    printf("Integer literal 143 (BIN):");
+    cout << 0b10001111 << endl;
+    
+
 
     return 0;
 }
