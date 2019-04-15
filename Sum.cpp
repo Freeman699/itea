334d9efc1733adb2755e8f3b4711cea803a4de28
Freeman699 <mr.frimen1324@gmail.com>
2019-04-15 14:13:51 +0300

    5th Final Sum.cpp Commit

--- Sum.cpp
+++ Sum.cpp
@@ -17,40 +17,28 @@ int main() {
 
     char FirstInputString[MAX_SIZE] {};
     char SecondInputString[MAX_SIZE] {};
-    
+  
+    auto LambdaInput = [](char InputSring[]) {
+StrEnterMark:
+        static int iter=1;
+        cout << "Please enter " << iter <<" number: ";
+        cin >> InputSring;
+        cout << endl;
+        
+        if(strlen(InputSring) >= MAX_SIZE - 2) {
+            cout << endl << "Your number might be too long\n Please retry..." << endl;
+            goto StrEnterMark;
+        }
+        else if(DigitCheckFunc(InputSring)) {
+            cout << endl << "Input error! Please retry..." << endl;
+            goto StrEnterMark;
+        }
 
+        ++iter;
+    };   
 
-StrFirstEnterMark:
-    cout << "Please enter first number: ";
-    cin >> FirstInputString;
-    cout << endl;
-    
-    if(strlen(FirstInputString) >= MAX_SIZE - 2) {
-        cout << endl << "Your number might be too long\n Please retry..." << endl;
-        goto StrFirstEnterMark;
-    }
-    else if(DigitCheckFunc(FirstInputString)) {
-        cout << endl << "Input error! Please retry..." << endl;
-        goto StrFirstEnterMark;
-    }
-
-
-
-StrSecondEnterMark:
-    cout << "Please enter second number: ";
-    cin >> SecondInputString;
-    cout << endl;
-    
-    if(strlen(SecondInputString) >= MAX_SIZE - 2) {
-        cout << endl << "Your number might be too long\n Please retry..." << endl;
-        goto StrSecondEnterMark;
-    }
-    else if(DigitCheckFunc(SecondInputString)) {
-        cout << endl << "Input error! Please retry..." << endl;
-        goto StrSecondEnterMark;
-    }
-
-
+    LambdaInput(FirstInputString);
+    LambdaInput(SecondInputString);
 
     if(strlen(FirstInputString) >= strlen(SecondInputString)) {
         SumFunc(FirstInputString,SecondInputString);
@@ -86,14 +74,9 @@ void SumFunc(char FNum[],char SNum[]) {
 
     for( ;k >= 0;--i, --k) {
         
-        // cout << "i = " << i << endl;
-
         uint32_t tempSum = 0,
                  firstNum = FNum[i] - '0',
                  secondNum = SNum[k] - '0';
-
-        // cout << "firstNum = " << firstNum << endl;
-        // cout << "secondNum = " << secondNum << endl;
 
         if(digit) {
             ++firstNum;
@@ -102,27 +85,21 @@ void SumFunc(char FNum[],char SNum[]) {
 
         tempSum = firstNum + secondNum;
         if(tempSum >= 10) {
-            // cout << "Is digit +" << endl;
+
             digit = true;
             tempSum %= 10;
         }
         
-        // cout << "tempSum = " << tempSum << endl;
         FNum[i] = tempSum + '0';
-        // cout << "FNum = " << FNum << endl;
     }
 
 
-
-        // cout << "FNum2 = " << FNum << endl;
-
-
     if(digit) {
-        for(int i = strlen(FNum); i > 0; --i) {
+        for(int i = strlen(FNum); i >= 0; --i) {
+            // cout << FNum[i] << endl;
             FNum[i+1] = FNum [i];
         }
 
-        // cout << "FNum3 = " << FNum << endl;
         FNum[0] = 1 + '0';
     }
 
